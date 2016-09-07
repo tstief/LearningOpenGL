8d8196f397d15648940edaa759fd1ad9ae3ee9d6
tstief <tstief@gmail.com>
2016-09-06 20:13:25 -0400

    Update to GLEW and GLFW build

--- HelloTriangle/main.cpp
+++ HelloTriangle/main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 
 // GLEW
-#define GLEW_STATIC
 #include <GL/glew.h>
 
 // GLFW
 #include <GLFW/glfw3.h>
-
 
 // Function prototypes
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
