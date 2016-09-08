ba5b3cb0d7c9f0b9feb3314fd0bcdc6a86d96734
tstief <tstief@gmail.com>
2016-09-08 00:50:37 -0400

    Code Formatting

--- Shaders/main.cpp
+++ Shaders/main.cpp
@@ -12,7 +12,7 @@
 #include "Shader.h"
 
 // Function prototypes
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void key_callback(GLFWwindow* window, int key, int scanCode, int action, int mode);
 
 // Window dimensions
 const GLuint WIDTH = 800, HEIGHT = 600;
@@ -51,9 +51,9 @@ int main()
   // Set up vertex data (and buffer(s)) and attribute pointers
   GLfloat vertices[] = {
       // Positions         // Colors
-      0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,  // Bottom Right
-      -0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,  // Bottom Left
-      0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f   // Top
+      0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // Bottom Right
+      -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // Bottom Left
+      0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f   // Top
   };
   GLuint VBO, VAO;
   glGenVertexArrays(1, &VAO);
@@ -65,10 +65,10 @@ int main()
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
   // Position attribute
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*) 0);
   glEnableVertexAttribArray(0);
   // Color attribute
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*) (3 * sizeof(GLfloat)));
   glEnableVertexAttribArray(1);
 
   glBindVertexArray(0); // Unbind VAO
@@ -103,8 +103,9 @@ int main()
 }
 
 // Is called whenever a key is pressed/released via GLFW
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+void key_callback(GLFWwindow* window, int key, int /*scanCode*/, int action, int /*mode*/)
 {
   if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     glfwSetWindowShouldClose(window, GL_TRUE);
 }
+
